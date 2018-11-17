4c04d77a464629ae21dc18655c315dcafa008202
pascal <pascal.keusch@stud.hslu.ch>
2018-11-17 11:26:57 +0100

    Create xcode project

---
+++ EventApp/EventApp/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  EventApp
+//
+//  Created by Pascal on 17.11.18.
+//  Copyright © 2018 2noobs. All rights reserved.
+//
+
+import Foundation
---
+++ EventApp/EventApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventApp
+//
+//  Created by Pascal on 17.11.18.
+//  Copyright © 2018 2noobs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
